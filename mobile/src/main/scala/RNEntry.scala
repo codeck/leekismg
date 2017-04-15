b346caef6a8e54f79b54a766334a02d9e512e783
Cheng Kuan <kring@dev.codeck.org>
2017-04-15 15:49:56 +0800

    more ES6 style of react

--- mobile/src/main/scala/RNEntry.scala
+++ mobile/src/main/scala/RNEntry.scala
@@ -3,17 +3,15 @@
   */
 
 import scala.scalajs.js
-import js.annotation.{JSExportDescendentClasses, JSExportDescendentObjects, JSImport}
-import js.Dynamic.{global => g}
+import js.annotation.{JSImport, JSName, ScalaJSDefined}
 import js.Dynamic.{literal => lit}
 
 @js.native
-@JSImport("react-native", "View")
-object View extends js.Object
-
-@js.native
-@JSImport("react-native", "Text")
-object Text extends js.Object
+@JSImport("react-native", JSImport.Namespace)
+object RN extends js.Object {
+  def View :js.Object = js.native
+  def Text :js.Object = js.native
+}
 
 @js.native
 @JSImport("react-native", "AppRegistry")
@@ -22,16 +20,37 @@ object AppRegistry extends js.Object {
 }
 
 @js.native
-@JSImport("react", JSImport.Default)
-object React extends js.Object {
-  def createClass(x :js.Object) :js.Object = js.native
-  def createElement(x :js.Any*) :js.Object = js.native
-}
-
-@js.native
 @JSImport("react-native", "NativeModules")
 object NativeModules extends js.Object {
   val CoreSMSModule :js.Dynamic = js.native
+}
+
+@js.native
+@JSImport("react", JSImport.Default)
+object React extends js.Object {
+  @JSName("createElement")
+  def <(x :js.Any*) :js.Object = js.native
+}
+
+@js.native
+@JSImport("react", "Component")
+class Component(val props :js.Dynamic) extends js.Object {
+  def setState(nextStateDiff :js.Object) :Unit = js.native
+  def setState(nextStateDiffFunc :(js.Dynamic /*previousState*/, js.Dynamic /*currentProps*/) => js.Object /*nextState*/) :Unit = js.native
+  def replaceState(nextStateFull :js.Object) :Unit = js.native
+
+  def forceUpdate() :Unit = js.native
+  def isMounted() :Boolean = js.native
+
+  def state :js.Dynamic = js.native
+  def render() :js.Object = js.native
+}
+
+@ScalaJSDefined
+class Hello(p :js.Dynamic) extends Component(p) {
+  override def render() = {
+    React.<(RN.Text, null, "From Hello Component, arg = " + props.arg)
+  }
 }
 
 object RNEntry extends js.JSApp {
@@ -39,16 +58,18 @@ object RNEntry extends js.JSApp {
   var somemsg = "..."
   NativeModules.CoreSMSModule.show((msg :String)=>somemsg = msg)
 
-    override def main() :Unit = {
-
-      val entry = React.createClass(lit(render = () =>
-        React.createElement(View, null,
-          React.createElement(Text, null, "Welcome to React Native!"),
-          React.createElement(Text, null, somemsg)
-        )
-      ));
-
-      AppRegistry.registerComponent("HelloWorldApp", () => entry);
-
+  @ScalaJSDefined
+  class EntryComponent (p :js.Dynamic) extends Component(p) {
+    override def render() = {
+      React.<(RN.View, null,
+        React.<(RN.Text, null, "Welcome to React Native!"),
+        React.<(js.constructorOf[Hello], lit(arg="<good>")),
+        React.<(RN.Text, null, somemsg)
+      )
     }
+  }
+
+  override def main() :Unit = {
+    AppRegistry.registerComponent("HelloWorldApp", () => js.constructorOf[EntryComponent]);
+  }
 }
